17fc5df9c85fccf77c0f00e8253a09651f889414
KingBobSkibidi <lucianbai0808@gmail.com>
2026-04-27 20:35:02 -0400

    April 27: up to 1.4
    - completed up to 1.4 of video, adding elephant and apple actor subclasses with apple falling down and elephant moving right

---
+++ Elephant.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * The elephant, our hero
+ * 
+ * @author Lucian Bai
+ * @version April 24, 2026
+ */
+public class Elephant extends Actor
+{
+    /**
+     * Act - do whatever the elephant wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        move(1);
+    }
+}

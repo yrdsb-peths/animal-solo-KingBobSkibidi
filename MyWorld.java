54472b633b14232b707bcee1928310b7c2ae17c3
KingBobSkibidi <lucianbai0808@gmail.com>
2026-04-27 21:15:32 -0400

    April 27: completed part 1
    - completed part 1, adding game over label that appears when apple touches floor and tweaking all heights (for apple, elephant and score label)

--- MyWorld.java
+++ MyWorld.java
@@ -11,17 +11,23 @@ public class MyWorld extends World {
     // constructor for objects of class MyWOrld
     public MyWorld() {
         // create a new world
-        super(600, 400, 1);
+        super(600, 400, 1, false);
         
         // create elephant object
         Elephant elephant = new Elephant();
-        addObject(elephant, 300, 300);
+        addObject(elephant, 300, 350);
         
         // create a label
         scoreLabel = new Label(0, 80);
-        addObject(scoreLabel, 50 , 50);
+        addObject(scoreLabel, 40 , 40);
         
         createApple();
+    }
+    // game over
+    public void gameOver() {
+        Label gameOverLabel = new Label("Game Over", 100);
+        addObject(gameOverLabel, 300, 200);
+        
     }
     
     // increase score
@@ -34,7 +40,6 @@ public class MyWorld extends World {
     public void createApple(){
         Apple apple = new Apple();
         int x = Greenfoot.getRandomNumber(600);
-        int y = Greenfoot.getRandomNumber(300);
-        addObject(apple, x, y);
+        addObject(apple, x, 90);
     }
 }

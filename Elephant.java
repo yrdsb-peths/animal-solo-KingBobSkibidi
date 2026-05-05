870111977d47b197d1b618f1e568c70d9fbd9f5f
KingBobSkibidi <lucianbai0808@gmail.com>
2026-05-05 09:11:26 -0400

    May 5: completed up to 2.8
    - added animation by iterating through an array of sprites
    - made elephant face left and right depending on which way elephant is moving

--- Elephant.java
+++ Elephant.java
@@ -9,24 +9,55 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Elephant extends Actor
 {
     GreenfootSound elephantSound = new GreenfootSound("elephant_trumpet.mp3");
-    GreenfootImage idle = new GreenfootImage("images/elephant_gif/sprite-4-1.png");
+    GreenfootImage[] idleRight = new GreenfootImage[10];
+    GreenfootImage[] idleLeft = new GreenfootImage[10];
+    
+    String facing = "right";
     
     //constructor
     public Elephant()
     {
-        setImage(idle);
+        for(int i=0; i < idleRight.length; i++){
+            idleRight[i] = new GreenfootImage("images/elephant_gif/sprite-4-" + (i+1) + ".png");
+            idleRight[i].scale(100, 100);
+        }
+        
+        for(int i=0; i < idleLeft.length; i++){
+            idleLeft[i] = new GreenfootImage("images/elephant_gif/sprite-4-" + (i+1) + ".png");
+            idleLeft[i].mirrorHorizontally();
+            idleLeft[i].scale(100, 100);
+        }
+        setImage(idleRight[0]);
     }
+    
+    //animate elephant
+    int imageIndex = 0;
+    public void animateElephant(){
+        if(facing.equals("right")) {
+            setImage(idleRight[imageIndex]);
+            imageIndex = (imageIndex + 1) % idleRight.length;
+        }
+        else {
+            setImage(idleLeft[imageIndex]);
+            imageIndex = (imageIndex + 1) % idleLeft.length;
+        }
+    }
+    
     
     public void act()
     {
         if(Greenfoot.isKeyDown("left")){
             move(-3);
+            facing = "left";
         }
         else if(Greenfoot.isKeyDown("right")){
             move(3);
+            facing = "right";
         }
         
         eat();
+        
+        animateElephant();
     }
     
     public void eat() {

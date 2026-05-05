6cc8a4c2460de846e7ccb1ce6430819a137a1351
KingBobSkibidi <lucianbai0808@gmail.com>
2026-05-05 19:06:23 -0400

    completed up to 3.4
    - added elephant image and labels for instructions on title page
    - moved around labels and title on title page
    - changed arrows in label to unicode characters

--- Titlescreen.java
+++ Titlescreen.java
@@ -17,10 +17,11 @@ public class Titlescreen extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
-        
-        addObject(titleLabel, getWidth()/2, getHeight()/2);
+
+        addObject(titleLabel, 240, 120);
+        prepare();
     }
-    
+
     public void act()
     {
         // start game if space bar pressed
@@ -28,6 +29,23 @@ public class Titlescreen extends World
             MyWorld gameWorld = new MyWorld();
             Greenfoot.setWorld(gameWorld);
         }
-        
+
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Elephant elephant = new Elephant();
+        addObject(elephant,430,141);
+        elephant.setLocation(512,102);
+        Label label = new Label("Press <Space> To Start", 40);
+        addObject(label,228,249);
+        label.setLocation(304,308);
+        Label label2 = new Label("Use \u2190 and \u2192 arrow keys to move", 25);
+        addObject(label2,164,233);
+        label2.setLocation(292,352);
     }
 }
